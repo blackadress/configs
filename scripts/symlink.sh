df14e29834e1ed31bd4067190aec36492ef60171
Erland <blackadress.01@gmail.com>
2019-03-28 07:32:27 -0500

    bash

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -2,3 +2,5 @@
 
 ln -sf ~/Documents/configs/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/Documents/configs/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf ~/Documents/configs/bash-configs/.bashrc ~/.bashrc
+ln -sf ~/Documents/configs/bash-configs/aliasrc ~/.config/aliasrc
