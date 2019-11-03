5c4e299373734607c0566335affa6b5e589ed783
blackadress <blackadress@github.com>
2019-11-03 17:47:53 -0500

    cambios menores

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -4,3 +4,5 @@ ln -sf ~/Documents/configs/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/Documents/configs/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf ~/Documents/configs/bash-configs/.bashrc ~/.bashrc
 ln -sf ~/Documents/configs/bash-configs/aliasrc ~/.config/aliasrc
+
+ln -sf ~/Documents/configs/scripts/chunks.sh ~/bin/chunks.sh
