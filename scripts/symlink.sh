8372cc88a74f7419777c8752c01af4552140aace
blackadress <blackadress@github.com>
2020-05-16 19:34:06 -0500

    .profile +inicio de mpd

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+# PROGRAMAS
+# configuracion nvim
 ln -sf ~/Documents/configs/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/Documents/configs/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -sf ~/Documents/configs/bash-configs/.bashrc ~/.bashrc
-ln -sf ~/Documents/configs/bash-configs/aliasrc ~/.config/aliasrc
+# configuracion tmux
 ln -sf ~/Documents/configs/tmux/tmux.conf ~/.tmux.conf
+## script de utilidad para tmux
+#ln -sf ~/Documents/configs/scripts/ide ~/bin/ide
 
+# configuracion terminal alacritty
+ln -sf ~/Documents/configs/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+# flux configs
 ln -sf ~/Documents/configs/scripts/xflux_setup.sh ~/bin/xflux_setup.sh
 ln -sf ~/Documents/configs/scripts/xflux_stop.sh ~/bin/xflux_stop.sh
 
+# BASH CONFIGS
+ln -sf ~/Documents/configs/bash-configs/.bashrc ~/.bashrc
+ln -sf ~/Documents/configs/bash-configs/aliasrc ~/.config/aliasrc
+ln -sf ~/Documents/configs/bash-configs/.profile ~/.profile
+
+# SCRIPTS
 ln -sf ~/Documents/configs/scripts/chunks.sh ~/bin/chunks.sh
-#ln -sf ~/Documents/configs/scripts/ide ~/bin/ide
+
