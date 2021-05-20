a220a0ec2c979a37048c0a0a258067f188295a59
blackadress <blackadress.01@gmail.com>
2021-05-20 15:35:59 -0500

    gitsigns manual config of colors

--- nvim/lua-configs/lua/plugconfigs.lua
+++ nvim/lua-configs/lua/plugconfigs.lua
@@ -27,12 +27,31 @@ vim.g.nvim_tree_bindings = {
 --vim.api.nvim_set_keymap('n', '<Leader>n', ':NvimTreeFindFile<CR>', { silent = true })
 --
 --GITSIGNS
-  -- You can define these with he highlight command. Somethling like this should work.
-  -- :highlight GitSignsAdd guifg=Blue https://github.com/lewis6991/gitsigns.nvim/issues/169
-  -- vim.cmd[[ highlight GitSignsAdd guifg=#2aa198]]
-  -- vim.cmd[[ highlight GitSignsChange guifg=#6c71c4]]
-  -- vim.cmd[[ highlight GitSignsDelete guifg=#d33682]]
-require("gitsigns").setup()
+-- You can define these with he highlight command. Somethling like this should work.
+-- :highlight GitSignsAdd guifg=Blue https://github.com/lewis6991/gitsigns.nvim/issues/169
+-- vim.cmd("highlight GitSignsAdd guifg=#2aa198")
+-- vim.cmd("highlight GitSignsChange guifg=#6c71c4") 
+-- vim.cmd("highlight GitSignsDelete guifg=#d33682")
+require("gitsigns").setup {
+  signs = {
+    add          = {hl = 'Constant'   , text = '│', numhl='Constant'   , linehl='Constant'},
+    change       = {hl = 'Underlined', text = '│', numhl='Underlined', linehl='Underlined'},
+    delete       = {hl = 'gitCommitBranch', text = '_', numhl='gitCommitBranch', linehl='gitCommitBranch'},
+    topdelete    = {hl = 'WarningMsg', text = '‾', numhl='WarningMsg', linehl='WarningMsg'},
+    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+  },
+  numhl = false,
+  linehl = false,
+  watch_index = {
+    interval = 1000
+  },
+  current_line_blame = false,
+  sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  use_decoration_api = true,
+  use_internal_diff = true,  -- If luajit is present
+}
 
 -- TELESCOPE
 require("telescope").setup {
@@ -200,4 +219,4 @@ require("formatter").setup(
 vim.api.nvim_set_keymap("n", "<Leader>ff", ":Format<CR>", {noremap = true, silent = true})
 
 --colorizer
-require'colorizer'.setup()
+require "colorizer".setup()
--- nvim/lua-configs/lua/plugins.lua
+++ nvim/lua-configs/lua/plugins.lua
@@ -1,66 +1,63 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+  fn.system({"git", "clone", "https://github.com/wbthomason/packer.nvim", install_path})
+  execute "packadd packer.nvim"
 end
 
-return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(
+  function()
+    use "wbthomason/packer.nvim"
+    use {"kyazdani42/nvim-tree.lua", requires = {"kyazdani42/nvim-web-devicons"}}
+    use {"lewis6991/gitsigns.nvim", requires = {"nvim-lua/plenary.nvim"}}
 
-  use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } }
-  use { 
-    'lewis6991/gitsigns.nvim',
-    requires = { 'nvim-lua/plenary.nvim' }  }
+    use "terrortylor/nvim-comment"
+    use {"nvim-telescope/telescope.nvim", requires = {{"nvim-lua/popup.nvim"}, {"nvim-lua/plenary.nvim"}}}
+    use {"nvim-telescope/telescope-fzy-native.nvim"}
+    -- falta configurar
+    use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+    use "mhartington/formatter.nvim"
 
-  use 'terrortylor/nvim-comment'
-  use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}}
-  use { 'nvim-telescope/telescope-fzy-native.nvim' }
-  -- falta configurar
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use 'mhartington/formatter.nvim'
+    use "neovim/nvim-lspconfig"
+    use "hrsh7th/vim-vsnip"
+    use "hrsh7th/nvim-compe"
 
-  use 'neovim/nvim-lspconfig'
-	use 'hrsh7th/vim-vsnip'
-  use 'hrsh7th/nvim-compe'
+    use "vim-airline/vim-airline"
+    use "vim-airline/vim-airline-themes"
 
-  use 'vim-airline/vim-airline'
-  use 'vim-airline/vim-airline-themes'
+    use "norcalli/nvim-colorizer.lua"
+    use "vimwiki/vimwiki"
 
-  use 'norcalli/nvim-colorizer.lua'
-  use 'vimwiki/vimwiki'
+    --color themes
+    use "romainl/flattened"
+    use "challenger-deep-theme/vim"
+    use "liuchengxu/space-vim-dark"
+    use "sainnhe/edge"
+    use "morhetz/gruvbox"
 
-  --color themes
-  use 'romainl/flattened'
-  use 'challenger-deep-theme/vim'
-  use 'liuchengxu/space-vim-dark'
-  use 'sainnhe/edge'
-  use 'morhetz/gruvbox'
+    use "tyrannicaltoucan/vim-deep-space"
+    use "wadackel/vim-dogrun"
+    use "danilo-augusto/vim-afterglow"
+    use "hachy/eva01.vim"
+    use "ajmwagar/vim-deus"
+    use "w0ng/vim-hybrid"
 
-  use 'tyrannicaltoucan/vim-deep-space'
-  use 'wadackel/vim-dogrun'
-  use 'danilo-augusto/vim-afterglow'
-  use 'hachy/eva01.vim'
-  use 'ajmwagar/vim-deus'
-  use 'w0ng/vim-hybrid'
+    use "glepnir/oceanic-material"
 
-  use 'glepnir/oceanic-material'
-
-  
-  -- Soporte sintactico de lenguaje
-  -- todavia no estoy usando el LSP nativo de neovim
-  -- falta configurar
-  use 'HerringtonDarkholme/yats.vim'
-  use 'MaxMEllon/vim-jsx-pretty'
-  use 'yuezk/vim-js'
-  use 'elmcast/elm-vim'
-  -- use { 'fatih/vim-go' } --, run = ':GoUpdateBinaries' 
-  use { 'numirias/semshi', run = ':UpdateRemote  useins' }
-  use 'rust-lang/rust.vim'
-  use 'habamax/vim-godot'
-  use 'neovimhaskell/haskell-vim'
-
-end)
+    -- Soporte sintactico de lenguaje
+    -- todavia no estoy usando el LSP nativo de neovim
+    -- falta configurar
+    use "HerringtonDarkholme/yats.vim"
+    use "MaxMEllon/vim-jsx-pretty"
+    use "yuezk/vim-js"
+    use "elmcast/elm-vim"
+    -- use { 'fatih/vim-go' } --, run = ':GoUpdateBinaries'
+    use {"numirias/semshi", run = ":UpdateRemote  useins"}
+    use "rust-lang/rust.vim"
+    use "habamax/vim-godot"
+    use "neovimhaskell/haskell-vim"
+  end
+)
