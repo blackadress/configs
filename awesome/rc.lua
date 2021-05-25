72996f36c6721b8d5986b505b59b0e9c917817a8
blackadress <blackadress.01@gmail.com>
2021-05-24 22:19:12 -0500

    awesome conf

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -60,8 +60,8 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
--- theme.font = 'Source Code Pro 10'
-beautiful.init(gears.filesystem.get_configuration_dir() .. "mytheme.lua")
+-- beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
+beautiful.init(gears.filesystem.get_configuration_dir() .. "theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
@@ -829,7 +829,7 @@ client.connect_signal(
   function(c)
     -- Set the windows at the slave,
     -- i.e. put it at the end of others instead of setting it master.
-    -- if not awesome.startup then awful.client.setslave(c) end
+    if not awesome.startup then awful.client.setslave(c) end
 
     if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
       -- Prevent clients from being unreachable after screen count changes.
