b9397680557d4d435847b63d0c8dae1a70028dcf
blackadress <blackadress.01@gmail.com>
2021-05-27 09:54:51 -0500

    tree siter first config, vim-go highlight settings

--- nvim/lua-configs/lua/languages.lua
+++ nvim/lua-configs/lua/languages.lua
@@ -1,54 +1,40 @@
---[[
-autocmd Filetype css setlocal tabstop=2 shiftwidth=2 expandtab
-autocmd Filetype html setlocal tabstop=2 shiftwidth=2 expandtab
-autocmd Filetype htmldjango setlocal tabstop=2 shiftwidth=2 expandtab
-autocmd Filetype javascript setlocal tabstop=2 shiftwidth=2 expandtab
-autocmd Filetype typescript setlocal tabstop=2 shiftwidth=2 expandtab
-autocmd Filetype tex setlocal tabstop=2 shiftwidth=2 expandtab
-autocmd Filetype plaintex setlocal tabstop=2 shiftwidth=2 expandtab
-autocmd Filetype typescriptreact setlocal tabstop=2 shiftwidth=2 expandtab
-autocmd Filetype javascriptreact setlocal tabstop=2 shiftwidth=2 expandtab
-
-autocmd Filetype go setlocal tabstop=4 shiftwidth=4
-autocmd Filetype rust setlocal tabstop=4 shiftwidth=4 expandtab
-autocmd Filetype python setlocal tabstop=4 shiftwidth=4 expandtab
-autocmd Filetype sql setlocal tabstop=4 shiftwidth=4 expandtab
-
-"omnifuncs
-augroup omnifuncs
-   autocmd!
-   autocmd FileType css setlocal omnifunc=csscomplete#CompleteCSS
-   autocmd FileType html,markdown setlocal omnifunc=htmlcomplete#CompleteTags
-   autocmd FileType javascript setlocal omnifunc=javascriptcomplete#CompleteJS
-   autocmd FileType python setlocal omnifunc=pythoncomplete#Complete
-   autocmd FileType xml setlocal omnifunc=xmlcomplete#CompleteTags
-augroup end
-]] --
 vim.cmd("autocmd FileType css setlocal tabstop=2 shiftwidth=2 expandtab")
 vim.cmd("autocmd FileType lua setlocal tabstop=2 shiftwidth=2 expandtab")
 vim.cmd("autocmd FileType html setlocal tabstop=2 shiftwidth=2 expandtab")
-vim.cmd("autocmd FileType haskell setlocal tabstop=2 shiftwidth=2 expandtab")
 vim.cmd("autocmd FileType htmldjango setlocal tabstop=2 shiftwidth=2 expandtab")
 vim.cmd("autocmd FileType javascript setlocal tabstop=2 shiftwidth=2 expandtab expandtab expandtab")
 vim.cmd("autocmd FileType javascriptreact setlocal tabstop=2 shiftwidth=2 expandtab expandtab")
 vim.cmd("autocmd FileType typescriptreact setlocal tabstop=2 shiftwidth=2 expandtab expandtab")
-vim.cmd("autocmd FileType tex setlocal tabstop=2 shiftwidth=2 expandtab expandtab")
 vim.cmd("autocmd FileType plaintext setlocal tabstop=2 shiftwidth=2 expandtab expandtab")
-
-vim.cmd("autocmd FileType go setlocal tabstop=4 shiftwidth=4")
-vim.cmd("autocmd FileType python setlocal tabstop=4 shiftwidth=4 expandtab")
-vim.cmd("autocmd FileType rust setlocal tabstop=4 shiftwidth=4 expandtab")
 vim.cmd("autocmd FileType sql setlocal tabstop=4 shiftwidth=4 expandtab")
 
+
 -- PYTHON
+vim.cmd("autocmd FileType python setlocal tabstop=4 shiftwidth=4 expandtab")
 vim.api.nvim_set_var("python3_host_prog", "/usr/bin/python")
 
 -- HASKELL
+vim.cmd("autocmd FileType haskell setlocal tabstop=2 shiftwidth=2 expandtab")
+
 -- GODOT
+-- GO
+vim.cmd("autocmd FileType go setlocal tabstop=4 shiftwidth=4")
+-- VIM-GO
+vim.cmd('let g:go_highlight_build_constraints = 1')
+vim.cmd('let g:go_highlight_extra_types = 1')
+vim.cmd('let g:go_highlight_fields = 1')
+vim.cmd('let g:go_highlight_functions = 1')
+vim.cmd('let g:go_highlight_methods = 1')
+vim.cmd('let g:go_highlight_operators = 1')
+vim.cmd('let g:go_highlight_structs = 1')
+vim.cmd('let g:go_highlight_types = 1')
+
 -- RUST
+vim.cmd("autocmd FileType rust setlocal tabstop=4 shiftwidth=4 expandtab")
 vim.api.nvim_set_var("rustfmt_autosave", 1)
 
 -- TEX
+vim.cmd("autocmd FileType tex setlocal tabstop=2 shiftwidth=2 expandtab expandtab")
 vim.api.nvim_set_var("tex_conceal", "")
 --autocmd FileType tex let b:coc_pairs = [["$", "$"]]
 --autocmd FileType tex let b:coc_pairs_disabled=["'", "\"", "`"]
